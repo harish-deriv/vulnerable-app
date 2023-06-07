35350a902f021ec030cf271dce3d4f491884b25a
harish-deriv <107453042+harish-deriv@users.noreply.github.com>
2023-06-07 10:47:57 +0800

    Update FileUpload.pm

--- app/lib/MyApp/Controller/FileUpload.pm
+++ app/lib/MyApp/Controller/FileUpload.pm
@@ -22,4 +22,31 @@ sub upload {
     $self->render(text => 'File uploaded successfully');
 }
 
+sub upload_svg {
+    my $self = shift;
+
+    # Get uploaded file
+    my $file = $self->req->upload('svg_file');
+    unless ($file) {
+        $self->render(text => 'No file uploaded');
+        return;
+    }
+
+    # Save the uploaded file
+    my $filename = $file->filename;
+    my $path = $self->app->home->rel_file("public/uploads/$filename");
+    $file->move_to($path);
+
+    # Process the SVG file
+    my $svg_content = $file->slurp;
+    $self->render_svg($svg_content);
+}
+
+sub render_svg {
+    my ($self, $svg_content) = @_;
+
+    # Render the SVG file in a template
+    $self->render(template => 'xxe/render_svg', svg_content => $svg_content);
+}
+
 1;
